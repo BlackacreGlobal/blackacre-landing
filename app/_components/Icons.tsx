177f453a15a2fa2ee146dd80d2706a3394feb54c
Keanu Kent B. Gargar <keanukent.dev@gmail.com>
2024-05-22 21:52:11 +0800

    use react syntax on svg

--- app/_components/Icons.tsx
+++ app/_components/Icons.tsx
@@ -61,8 +61,8 @@ export const ArrowUpIcon = React.forwardRef<SVGSVGElement, IconProps>(
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="m15 11.25-3-3m0 0-3 3m3-3v7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
       />
     </svg>
