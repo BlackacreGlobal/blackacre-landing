d31e48b7b5a7585d6e819c2493e5d3f22415bc38
Hamza6-hash <mastank387@gmail.com>
2024-06-06 23:11:29 +0500

    responsivness fix

--- app/_components/About/AboutPart1.tsx
+++ app/_components/About/AboutPart1.tsx
@@ -1,17 +1,16 @@
-import PageHeading from "@/app/_components/PageHeading";
 import AboutLayout from "./AboutLayout";
 
-export default function AboutPart1({ }) {
+export default function AboutPart1({}) {
   return (
     <div className="w-full max-w-[64rem] flex flex-col items-center mx-auto">
       <AboutLayout>
-        <p>
+        <p className="max-md:text-base max-md:whitespace-break-spaces">
           At Blackacre, we're not just another IT company – we're your trusted
           partner in navigating the digital landscape. With a relentless
           commitment to excellence, we strive to deliver innovative solutions
           tailored to meet your unique needs.
         </p>
-        <p>
+        <p className="max-md:text-base max-md:whitespace-break-spaces">
           Our journey began with a vision to revolutionize the way businesses
           harness technology, and today, we stand at the forefront of the
           industry, empowering organizations of all sizes to thrive in the
--- app/_components/VerticalCarousel.tsx
+++ app/_components/VerticalCarousel.tsx
@@ -64,7 +64,7 @@ export default function VerticalCarousel({ }) {
         <PageHeading heading="ABOUT" subHeading="US" />
       </div>
       <div
-        className="absolute md:-top-[46px] top-[120px] left-0 h-full w-full"
+        className="absolute md:-top-[46px] left-0 h-full w-full"
         style={{
           transform: `translateY(-${currentPage * 100}%)`,
           transition: "transform 0.3s ease-in-out",
